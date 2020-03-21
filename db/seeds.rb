8c28231e652307b58fe51130a8583dca9167add8
Piotr Brzózka <weemanjz@gmail.com>
2020-03-21 20:58:46 +0100

    update help requests locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -124,18 +124,22 @@ users = User.all.sample(10)
 
 HelpRequest.create(user: User.john_tourist, title: 'Making masks for University Hospital in Cracow',
                    description: FFaker::Lorem.paragraph, target: 3000, unit: 'masks', request_type: :crowd,
-                   lat: rand(50.050000..50.050100), lng: rand(19.947030..19.947100))
+                   lat: 50.049273, lng: 19.944699)
 HelpRequest.create(user: users.shift, title: 'Shopping for Mr. Eugen', time_from: '2020-03-21 14:30', time_to: '2020-03-21 18:30',
                    description: FFaker::Lorem.paragraph, request_type: :single,
-                   lat: rand(50.050000..50.050100), lng: rand(19.947030..19.947100))
+                   lat: 50.050796, lng: 19.945749)
 HelpRequest.create(user: users.shift, title: 'Walk a dog', time_from: '2020-03-21 08:30', time_to: '2020-03-21 10:00',
                    description: FFaker::Lorem.paragraph, request_type: :single,
-                   lat: rand(50.050000..50.050100), lng: rand(19.947030..19.947100))
+                   lat: 50.050276, lng: 19.948313)
 HelpRequest.create(user: users.shift, title: 'Buy a medicine', time_from: '2020-03-20 12:00', time_to: '2020-03-20 16:00',
                    description: FFaker::Lorem.paragraph, request_type: :single,
-                   lat: rand(50.050000..50.050100), lng: rand(19.947030..19.947100))
+                   lat: 50.051268, lng: 19.946891)
+locations = [[50.051051, 19.948393], [50.052325, 19.949294], [50.050393, 19.943881],
+             [50.049367, 19.942776], [50.050358, 19.942824], [50.050888, 19.951155],
+             [50.051856, 19.950667]]
 users.each do |user|
+  loc = locations.shift
   HelpRequest.create(user: user, title: FFaker::Lorem.sentence, request_type: :crowd,
                      description: FFaker::Lorem.paragraph, target: rand(5000), unit: FFaker::Lorem.word,
-                     lat: rand(50.050000..50.050100), lng: rand(19.947030..19.947100))
+                     lat: loc[0], lng: loc[1])
 end
